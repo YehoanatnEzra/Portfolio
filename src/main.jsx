4ace5f5af27f8dcd9a312cd62d6bfca244611df5
YehoanatnEzra <yonzra12@gmail.com>
2025-08-21 12:09:34 +0300

    split CSS files instead of the single

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './styles.css'
+import './styles/base.css'
+import './styles/layout.css'
+import './styles/components.css'
+import './styles/home.css'
+import './styles/projects.css'
+import './styles/athletics.css'
+import './styles/resume.css'
+import './styles/responsive.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
